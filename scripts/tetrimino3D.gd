54997293a871a3903ffc17d6d1df7d4938942191
rebdal1998 <rebedalt@iu.edu>
2023-04-24 12:43:56 -0400

    Update
    - Added collision to the tetriminoes.
    - Adjusted camera in main scene

--- scripts/tetrimino3D.gd
+++ scripts/tetrimino3D.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
-
+ 
 func _physics_process(delta):
+	if not is_on_floor(): 
+		velocity.y = -5
 	if Input.is_action_just_pressed("rotate-x-cw"): 
 		self.rotate_x( -PI / 2)
 	elif Input.is_action_just_pressed("rotate-x-ccw"): 
