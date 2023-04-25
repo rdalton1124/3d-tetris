eca3392bc08b6c011a17f16b98eb811c9ddd06ef
Dalton, Rebecca Lynne <rebedalt@iu.edu>
2023-04-25 00:11:47 -0400

    Added wall
    - created wall boject, static body with collider
    - Added 4 walls on all sides of well
    - Added code to tetrimino to keep them from moving if it's colliding with wall.
    -

--- scripts/tetrimino3D.gd
+++ scripts/tetrimino3D.gd
@@ -1,11 +1,18 @@
 extends CharacterBody3D
 @onready var root = get_parent()
 @onready var spwner = $"../Spawner"
-
+# collision variables 
+var collidingLeft
+var collidingRight 
+var collidingTop 
+var collidingBottom 
 
 func _ready():
 	print("Tetrimino ready called")
-	
+	collidingLeft = false
+	collidingRight = false
+	collidingTop = false 
+	collidingBottom = false 
 func _physics_process(delta):
 	if not is_on_floor(): 
 		if Input.is_action_pressed("speed_up"):
@@ -32,12 +39,27 @@ func _physics_process(delta):
 	elif Input.is_action_just_pressed("rotate-z-ccw"): 
 		self.rotate_z(PI / 2)
 
-	if Input.is_action_just_pressed("move_forward"):
+	if Input.is_action_just_pressed("move_forward") and not collidingTop:
 		self.position.z -= .4
-	elif Input.is_action_just_pressed("move_backward"):
+	elif Input.is_action_just_pressed("move_backward") and not collidingBottom:
 		self.position.z += .4
-	if Input.is_action_just_pressed("move_left"):
+	if Input.is_action_just_pressed("move_left") and not collidingLeft:
 		self.position.x -= .4
-	elif Input.is_action_just_pressed("move_right"): 
+	elif Input.is_action_just_pressed("move_right") and not collidingRight: 
 		self.position.x += .4
 	move_and_slide()
+	collidingLeft = false 
+	collidingRight = false
+	collidingTop = false
+	collidingBottom = false 
+	for i in range(get_slide_collision_count()):
+		if get_slide_collision(i).get_collider().name == "wall_left":
+			collidingLeft = true
+		elif get_slide_collision(i).get_collider().name == "wall_right":
+			collidingRight = true
+		if get_slide_collision(i).get_collider().name == "wall_top": 
+			collidingTop = true 
+		elif get_slide_collision(i).get_collider().name == "wall_bottom":
+			collidingBottom = true
+		
+	
